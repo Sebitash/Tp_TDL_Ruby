e64f8864e7dbfa9033b207590bcaba1196dde66b
Francisco Antonio Pereyra <fpereyra@fi.uba.ar>
2024-05-14 12:31:53 -0300

    ejemplo de ruby2d para ver que nos ande

---
+++ ejemplo_ruby2d.rb
@@ -0,0 +1,16 @@
+# Ejemplo de Ruby2D para que veamos que nos anda bien
+# Lo corren con:
+#   ruby triangle.rb
+
+require 'ruby2d'
+
+set title: "Hello Triangle"
+
+Triangle.new(
+  x1: 320, y1:  50,
+  x2: 540, y2: 430,
+  x3: 100, y3: 430,
+  color: ['red', 'green', 'blue']
+)
+
+show
