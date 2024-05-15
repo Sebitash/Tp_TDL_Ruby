b0a9c0bf2e3c14e8cd4694a9b415432813090878
RodoAlbornoz <ralbornozt@fi.uba.ar>
2024-05-15 17:08:23 -0300

    Modularizar personaje y pasarlo a otro archivo

--- src/mapa.rb
+++ src/mapa.rb
@@ -1,71 +1,14 @@
 require 'ruby2d'
+require_relative 'personaje'
 
 #set fps_cap: 30
 set width: 800
 set height: 600
 
-PASO = 15
-
-IMAGEN_ANCHO = 3072
-IMAGEN_ALTO = 1536
-
-SPRITE_ANCHO = 256
-SPRITE_ALTO = 256
-
-def moverse_a_izquierda(heroe, fondo)
-  heroe.play flip: :horizontal
-
-  if fondo.x < 0
-    fondo.x += PASO
-  elsif heroe.x > -50
-    heroe.x -= PASO
-  end
-end
-
-def moverse_a_derecha(heroe, fondo)
-  heroe.play
-
-  if (fondo.x - Window.width) > -IMAGEN_ANCHO
-    fondo.x -= PASO
-  elsif heroe.x < (Window.width - SPRITE_ANCHO)
-    heroe.x += PASO
-  end
-end
-
-def moverse_arriba(heroe, fondo)
-  heroe.play
-
-  if fondo.y < 0 and heroe.y > 0
-    fondo.y += PASO
-  elsif heroe.y > 0
-    heroe.y -= PASO
-  end
-end
-
-def moverse_abajo(heroe, fondo)
-  heroe.play
-
-  if (fondo.y - Window.height) > - fondo.height
-    fondo.y -= PASO
-  elsif heroe.y < (Window.height - SPRITE_ALTO)
-    heroe.y += PASO
-  end
-end
-
 def main
   fondo = Image.new(
-  'fondo.png',
-  y: -920
-  )
-
-  heroe = Sprite.new(
-    'heroe.png',
-    # heroee se separa en 4 animaciones de 256 cada una (256 x 4 = 1024, una dimension de la imagen)
-    width: SPRITE_ANCHO,
-    height: SPRITE_ALTO,
-    clip_width: SPRITE_ANCHO,
-    x: (Window.width - SPRITE_ANCHO) / 2,
-    y: (Window.height - SPRITE_ALTO) / 2
+    'fondo.png',
+    y: -920
   )
 
   mensaje = Text.new(
@@ -75,23 +18,25 @@ def main
     z: 1 # Para que aparezca sobre el fondo
   )
 
+  personaje = Personaje.new
+
   on :key_held do |event|
     mensaje.remove
 
     case event.key
     when 'left'
-      moverse_a_izquierda(heroe, fondo)
+      personaje.moverse_a_izquierda(fondo)
     when 'right'
-      moverse_a_derecha(heroe, fondo)
+      personaje.moverse_a_derecha(fondo)
     when 'up'
-      moverse_arriba(heroe, fondo)
+      personaje.moverse_arriba(fondo)
     when 'down'
-      moverse_abajo(heroe, fondo)
+      personaje.moverse_abajo(fondo)
     end
   end
 
   on :key_up do
-    heroe.stop
+    personaje.parar
   end
 
   show
