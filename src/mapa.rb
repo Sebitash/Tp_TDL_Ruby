e607479146a2828a3db04ae66d63079cf14f05ca
RodoAlbornoz <ralbornozt@fi.uba.ar>
2024-05-15 16:25:08 -0300

    Modularizar movimiento personaje y crear main

--- src/mapa.rb
+++ src/mapa.rb
@@ -1,81 +1,100 @@
 require 'ruby2d'
 
 #set fps_cap: 30
-
-PASO = 15
-
 set width: 800
 set height: 600
 
-background = Image.new(
-  'background.png',
-  y: -920
-)
+PASO = 15
 
-imagen_ancho = 3072
-imagen_alto = 1536
+IMAGEN_ANCHO = 3072
+IMAGEN_ALTO = 1536
 
-sprite_ancho = 256
-sprite_alto = 256
+SPRITE_ANCHO = 256
+SPRITE_ALTO = 256
 
-hero = Sprite.new(
-  'hero.png',
-  # Heroe se separa en 4 animaciones de 256 cada una (256 x 4 = 1024, una dimension de la imagen)
-  width: sprite_ancho,
-  height: sprite_alto,
-  clip_width: sprite_ancho,
-  x: (Window.width - sprite_ancho) / 2,
-  y: (Window.height - sprite_alto) / 2
-)
+def moverse_a_izquierda(heroe, fondo)
+  heroe.play flip: :horizontal
 
-mensaje = Text.new(
-  "Presione cualquiera tecla para comenzar",
-  x: (Window.width - 350) / 2,
-  y: (Window.height - 20) / 2,
-  z: 1 # Para que aparezca sobre el background
-)
-
-on :key_held do |event|
-  mensaje.remove
-
-  case event.key
-  when 'left'
-    hero.play flip: :horizontal
-
-    if background.x < 0
-      background.x += PASO
-    elsif hero.x > -50
-      hero.x -= PASO
-    end
-  when 'right'
-    hero.play
-
-    if (background.x - Window.width) > -imagen_ancho
-      background.x -= PASO
-    elsif hero.x < (Window.width - sprite_ancho)
-      hero.x += PASO
-    end
-  when 'up'
-    hero.play
-
-    if background.y < 0 and hero.y > 0
-      background.y += PASO
-    elsif hero.y > 0
-      hero.y -= PASO
-    end
-  when 'down'
-    hero.play
-
-    if (background.y - Window.height) > - background.height
-      background.y -= PASO
-    elsif hero.y < (Window.height - sprite_alto)
-      hero.y += PASO
-    end
+  if fondo.x < 0
+    fondo.x += PASO
+  elsif heroe.x > -50
+    heroe.x -= PASO
   end
 end
 
-on :key_up do
-  hero.stop
+def moverse_a_derecha(heroe, fondo)
+  heroe.play
+
+  if (fondo.x - Window.width) > -IMAGEN_ANCHO
+    fondo.x -= PASO
+  elsif heroe.x < (Window.width - SPRITE_ANCHO)
+    heroe.x += PASO
+  end
 end
 
-show
+def moverse_arriba(heroe, fondo)
+  heroe.play
+
+  if fondo.y < 0 and heroe.y > 0
+    fondo.y += PASO
+  elsif heroe.y > 0
+    heroe.y -= PASO
+  end
+end
+
+def moverse_abajo(heroe, fondo)
+  heroe.play
+
+  if (fondo.y - Window.height) > - fondo.height
+    fondo.y -= PASO
+  elsif heroe.y < (Window.height - SPRITE_ALTO)
+    heroe.y += PASO
+  end
+end
+
+def main
+  fondo = Image.new(
+  'fondo.png',
+  y: -920
+  )
+
+  heroe = Sprite.new(
+    'heroe.png',
+    # heroee se separa en 4 animaciones de 256 cada una (256 x 4 = 1024, una dimension de la imagen)
+    width: SPRITE_ANCHO,
+    height: SPRITE_ALTO,
+    clip_width: SPRITE_ANCHO,
+    x: (Window.width - SPRITE_ANCHO) / 2,
+    y: (Window.height - SPRITE_ALTO) / 2
+  )
+
+  mensaje = Text.new(
+    "Presione cualquiera tecla para comenzar",
+    x: (Window.width - 350) / 2,
+    y: (Window.height - 20) / 2,
+    z: 1 # Para que aparezca sobre el fondo
+  )
+
+  on :key_held do |event|
+    mensaje.remove
+
+    case event.key
+    when 'left'
+      moverse_a_izquierda(heroe, fondo)
+    when 'right'
+      moverse_a_derecha(heroe, fondo)
+    when 'up'
+      moverse_arriba(heroe, fondo)
+    when 'down'
+      moverse_abajo(heroe, fondo)
+    end
+  end
+
+  on :key_up do
+    heroe.stop
+  end
+
+  show
+end
+
+main
