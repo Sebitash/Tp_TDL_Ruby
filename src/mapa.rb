fd0970e8474ceff270418c2cce5c2fb8ee171623
RodoAlbornoz <ralbornozt@fi.uba.ar>
2024-05-15 01:32:13 -0300

    Españolizar y añadir mas comentarios

--- src/mapa.rb
+++ src/mapa.rb
@@ -1,17 +1,25 @@
 require 'ruby2d'
 
-WALK_SPEED = 25
+PASO = 25
 
 set width: 800
 set height: 600
 
-background = Image.new('background.png', y: -920)
+background = Image.new(
+  'background.png',
+  y: -920
+)
 
 juego_empezado = false
 #tecla_presionada = false
 cuadrado = nil
 
-mensaje = Text.new("Clickear para empezar", x: Window.width / 2 - 90, y: Window.height / 2, z: 1)
+mensaje = Text.new(
+  "Clickear para empezar",
+  x: Window.width / 2 - 90,
+  y: Window.height / 2,
+  z: 1 # Para que aparezca sobre el background
+)
 
 on :mouse_down do |event|
   # Si ya existe cuadrado no añado otro mas, si ya toqué el mensaje creo cuadrado
@@ -38,35 +46,35 @@ on :key_held do |event|
   end
 
   #if !tecla_presionada
-    tecla_presionada = true
-    if event.key == 'left'
-      if cuadrado.x > 0
-        cuadrado.x -= WALK_SPEED
-      elsif background.x < 0
-        background.x += WALK_SPEED
-      end
+  #tecla_presionada = true
+  if event.key == 'left'
+    if cuadrado.x > 0
+      cuadrado.x -= PASO
+    elsif background.x < 0
+      background.x += PASO
     end
-    if event.key == 'right'
-      if cuadrado.x < (Window.width - cuadrado.size)
-        cuadrado.x += WALK_SPEED
-      elsif (background.x - Window.width) > -background.width
-        background.x -= WALK_SPEED
-      end
+  end
+  if event.key == 'right'
+    if cuadrado.x < (Window.width - cuadrado.size)
+      cuadrado.x += PASO
+    elsif (background.x - Window.width) > -background.width
+      background.x -= PASO
     end
-    if event.key == 'up'
-      if cuadrado.y > 0
-        cuadrado.y -= WALK_SPEED
-      elsif background.y < 0
-        background.y += WALK_SPEED
-      end
+  end
+  if event.key == 'up'
+    if cuadrado.y > 0
+      cuadrado.y -= PASO
+    elsif background.y < 0
+      background.y += PASO
     end
-    if event.key == 'down'
-      if cuadrado.y < (Window.height - cuadrado.size)
-        cuadrado.y += WALK_SPEED
-      elsif (background.y - Window.height) > - background.height
-        background.y -= WALK_SPEED
-      end
+  end
+  if event.key == 'down'
+    if cuadrado.y < (Window.height - cuadrado.size)
+      cuadrado.y += PASO
+    elsif (background.y - Window.height) > - background.height
+      background.y -= PASO
     end
+  end
   #end
 end
 
