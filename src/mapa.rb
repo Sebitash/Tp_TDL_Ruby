6b785324997c3271002928fb6df1ecccb50d814f
RodoAlbornoz <ralbornozt@fi.uba.ar>
2024-05-15 00:30:47 -0300

    Fix bug al tocar flechas antes de clickear mensaje

--- src/mapa.rb
+++ src/mapa.rb
@@ -3,10 +3,17 @@ require 'ruby2d'
 WALK_SPEED = 5
 
 juego_empezado = false
-
 mensaje = Text.new("Clickear para empezar", x: Window.width / 2, y: Window.height / 2)
-
 cuadrado = nil
+
+/
+background = Image.new(
+  'basic.png',
+  width: Window.width,
+  height: Window.height,
+  z: 10
+)
+/
 
 on :mouse_down do |event|
   if juego_empezado and mensaje.contains?(event.x, event.y)
@@ -23,18 +30,20 @@ on :mouse_down do |event|
   end
 end
 
-on :key_held do |event|
-  if event.key == 'left'
-    cuadrado.x -= WALK_SPEED
-  end
-  if event.key == 'right'
-    cuadrado.x += WALK_SPEED
-  end
-  if event.key == 'up'
-    cuadrado.y -= WALK_SPEED
-  end
-  if event.key == 'down'
-    cuadrado.y += WALK_SPEED
+if juego_empezado
+  on :key_held do |event|
+    if event.key == 'left'
+      cuadrado.x -= WALK_SPEED
+    end
+    if event.key == 'right'
+      cuadrado.x += WALK_SPEED
+    end
+    if event.key == 'up'
+      cuadrado.y -= WALK_SPEED
+    end
+    if event.key == 'down'
+      cuadrado.y += WALK_SPEED
+    end
   end
 end
 
