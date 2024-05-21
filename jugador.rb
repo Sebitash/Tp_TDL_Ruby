3c3f44a1b602e60d49d7b32beb37ac2c2574dcdc
Francisco Antonio Pereyra <fpereyra@fi.uba.ar>
2024-05-20 23:55:40 -0300

    agregado rango de salud y de mana

--- jugador.rb
+++ jugador.rb
@@ -1,24 +1,28 @@
 require_relative 'equipamiento/armas'
 
 class Jugador
-  attr_reader :nombre, :pv
+  attr_reader :nombre
 
   def initialize(nombre, arma = Punios.new)
     @nombre = nombre
-    @pv = 100  # Puntos de vida
-    @pm = 100  # Puntos de mana
+    @pv = [100, 100]  # Puntos de vida
+    @pm = [100, 100]  # Puntos de mana
     @oro = 0
     @nivel = 1
     @arma = arma
-    @exp = 0
+    @exp = [0, 150]
   end
 
   def to_s
-    if @pv <= 0
+    if @pv[0] <= 0
       "El jugador ha muerto. Fin de la partida."
     else
-      "#{@nombre}: #{@pv}/100 PV | #{@pm}/100 PM | Nivel: #{@nivel} | #{@exp}/150 EXP | #{@oro} G\nArma equipada: #{@arma.to_s}"
+      "#{@nombre}: #{@pv[0]}/#{@pv[1]} PV | #{@pm[0]}/#{@pm[1]} PM | Nivel: #{@nivel} | #{@exp[0]}/#{@exp[1]} EXP | #{@oro} G\nArma equipada: #{@arma.to_s}"
     end
+  end
+
+  def pv
+    @pv[0]
   end
 
   def saludar
@@ -31,10 +35,10 @@ class Jugador
   end
 
   def atacar(objetivo)
-    if @pv <= 0
+    if @pv[0] <= 0
       return
     end
-    if objetivo.salud <= 0
+    if objetivo.pv <= 0
       puts "El objetivo ya esta muerto."
       return
     end
@@ -45,17 +49,19 @@ class Jugador
 
     if pv_restante_objetivo <= 0
       exp_ganada = objetivo.exp
-      @exp += exp_ganada
+      oro_ganado = objetivo.oro
+      @exp[0] += exp_ganada
+      @oro += oro_ganado
       puts "¡#{@nombre} ha asesinado a #{objetivo.nombre}!"
-      puts "¡#{@nombre} ha ganado #{exp_ganada} EXP!"
+      puts "¡#{@nombre} ha ganado #{exp_ganada} EXP y #{oro_ganado} G!"
     end
   end
 
   def recibir_ataque(ataque_enemigo)
-    @pv -= ataque_enemigo
+    @pv[0] -= ataque_enemigo
     puts "#{@nombre} recibe #{ataque_enemigo} puntos de daño."
-    if @pv <= 0
-      @pv = 0
+    if @pv[0] <= 0
+      @pv[0] = 0
       puts "¡El jugador ha muerto en batalla! Fin de la partida."
     end
   end
